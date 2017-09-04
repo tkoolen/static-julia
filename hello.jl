d1d56e222d036b5385349964c2fecc34c86dc7ad
Twan Koolen <koolen.twan@gmail.com>
2017-09-04 12:01:36 -0400

    RBD demo.

--- hello.jl
+++ hello.jl
@@ -1,14 +1,19 @@
-module mystuff
+module RBDMassMatrix
 
-using Gadfly
-using Distributions
-f() = rand(Normal())
+using RigidBodyDynamics
 
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
-    println("hello, world")
-    @show f()
-    plot(x=1:10, y=1:10)
-    return 0
+    mechanism = rand_tree_mechanism(Float64, [QuaternionFloating{Float64}; [Revolute{Float64} for i = 1 : 5]; [Fixed{Float64} for i = 1 : 5]; [QuaternionSpherical{Float64} for i = 1 : 5]; [Prismatic{Float64} for i = 1 : 5]; [Planar{Float64} for i = 1 : 5]]...)
+    state = MechanismState(mechanism)
+    rand!(state)
+    M = mass_matrix(state)
+    println(M)
+    0
 end
 
-end
+end # module
+
+using RBDMassMatrix
+
+RBDMassMatrix.julia_main(String[])
+
